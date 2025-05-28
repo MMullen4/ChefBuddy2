435fe414d3277830cbab07d8dd4f5740104a9535
mcclin53 <dan.mcclintic@gmail.com>
2025-05-27 20:42:14 -0400

    running API version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -86,6 +86,7 @@ function App() {
           <>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/faq">FAQ</Link></li>
           </>
         )}
       </ul>
