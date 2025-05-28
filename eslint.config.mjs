ad9f7810107a14580b44c519c3a8e990fce296bf
clinton reynolds <eastwood188209@gmail.com>
2025-05-27 22:10:44 -0400

    fixin gerrors linting

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-
-import eslintPlugin from '@eslint/js';
-
-export default [
-  {
-    ...eslintPlugin.configs.recommended, // ✅ Correct way to extend base ESLint rules
-    files: ['server/src/**/*.{js,ts,tsx}'],
-  },
-];
