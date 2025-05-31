c95fc76abfa4923049dfb64ce85209ab383eda30
Joseph Mueller <joseph.m.email@gmail.com>
2025-05-31 15:20:47 -0400

    delete profile from menu, deleted meal plan, saved fav divs

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import RecipeGenerator from "../components/generateRecipes";
-import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  const navigate = useNavigate();
 
   return (
     <main className="home-container">
@@ -18,27 +17,11 @@ const Home = () => {
       </div>
 
       <div className="row">
-        <div className="col s12 m4">
+        <div className="col s12">
           <div className="card">
             <div className="card-content">
               <span className="card-title deep-orange-text">Find Recipes<RecipeGenerator/></span>
               <p>Discover new recipes based on ingredients you have at home.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col s12 m4">
-          <div className="card">
-            <div className="card-content">
-              <button onClick={() => navigate('/favorites')} className="card-title deep-orange-text">Save Favorites</button>
-              <p>Store your favorite recipes for quick access later.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col s12 m4">
-          <div className="card">
-            <div className="card-content">
-              <span className="card-title deep-orange-text">Meal Planning</span>
-              <p>Plan your meals for the week with our easy-to-use tools.</p>
             </div>
           </div>
         </div>
