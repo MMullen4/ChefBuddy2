21ed658ff953c8a73e902314f35d6641fab51ef2
JayC-06 <Contreras.josh06@gmail.com>
2025-06-02 00:00:13 -0400

    fixed faq card and recognition card as well as styling and burger

--- client/src/pages/Recognition.tsx
+++ client/src/pages/Recognition.tsx
@@ -1,22 +1,4 @@
 const Recognition = () => {
-<<<<<<< HEAD
-    return (
-        <main className="recognition-container">
-        <div className="section">
-        <div className="row">
-        <div className="col s12">
-          <div className="card card-top">
-            <div className="card-content">
-              <span className="card-title deep-orange-text">The Recognition award goes to...</span>
-              <p>JOHN BROWN!!!</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-    </main>
-    );
-=======
   return (
    <main
   className="recognition-container"
@@ -35,7 +17,6 @@ const Recognition = () => {
   </div>
 </main>
   );
->>>>>>> main
 };
 
 export default Recognition;
