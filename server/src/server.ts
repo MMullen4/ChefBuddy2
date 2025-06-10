bb3ffd7062963b04a18e982505242af4e2b85116
Matt Mullen <mhmullen@icloud.com>
2025-06-10 10:15:32 -0700

    rev server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,9 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: path.resolve(__dirname, '../.env') });
+}
 
 import jwt from 'jsonwebtoken';
 
